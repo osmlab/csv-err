384dd4732b719bf7a603d110441d6ff9f1a53d3c
Aaron Lidman <aaronlidman@gmail.com>
2014-09-05 16:26:17 -0700

    Plug in npsdiff

--- src/tasks.npsdiff.sh
+++ src/tasks.npsdiff.sh
@@ -1,5 +1,5 @@
 mkdir npsdiff-tasks
 
 echo "
-    COPY (select ST_AsText(wkb_geometry), name, way from ogrgeojson) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from ogrgeojson) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres npsdiff > npsdiff-tasks/npsdiff.csv

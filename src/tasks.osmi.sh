cf8f6ca5d564cfe34a7227341319553baf93d3af
Aaron Lidman <aaronlidman@gmail.com>
2014-09-17 15:38:16 -0700

    order by random()

--- src/tasks.osmi.sh
+++ src/tasks.osmi.sh
@@ -3,41 +3,41 @@ set -e -u
 mkdir osmi-tasks
 
 echo "
-    COPY (select ST_AsText(wkb_geometry) from intersection_lines) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from intersection_lines order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/intersection_lines.csv
 
 echo "
-    COPY (select ST_AsText(wkb_geometry) from intersections) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from intersections order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/intersections.csv
 
 echo "
-    COPY (select ST_AsText(wkb_geometry) from role_mismatch) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from role_mismatch order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch.csv
 
 echo "
-    COPY (select ST_AsText(wkb_geometry) from role_mismatch_hull) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from role_mismatch_hull order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/role_mismatch_hull.csv
 
 echo "
-    COPY (select way_id, node_id from unconnected_major1) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select way_id, node_id from unconnected_major1 order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major1.csv
 
 echo "
-    COPY (select way_id, node_id from unconnected_major2) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select way_id, node_id from unconnected_major2 order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major2.csv
 
 echo "
-    COPY (select way_id, node_id from unconnected_major5) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select way_id, node_id from unconnected_major5 order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_major5.csv
 
 echo "
-    COPY (select way_id, node_id from unconnected_minor1) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select way_id, node_id from unconnected_minor1 order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_minor1.csv
 
 echo "
-    COPY (select way_id, node_id from unconnected_minor2) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select way_id, node_id from unconnected_minor2 order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/unconnected_minor2.csv
 
 echo "
-    COPY (select ST_AsText(wkb_geometry) from islands) to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry) from islands order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres osmi > osmi-tasks/islands.csv
--- src/tasks.tigerdelta.sh
+++ src/tasks.tigerdelta.sh
@@ -1,9 +1,9 @@
 mkdir tigerdelta-tasks
 
 echo "
-    COPY (select ST_AsText(wkb_geometry), name, way from ogrgeojson where name != '') to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry), name, way from ogrgeojson where name != '' order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres tigerdelta > tigerdelta-tasks/tigerdelta-named.csv
 
 echo "
-    COPY (select ST_AsText(wkb_geometry), name, way from ogrgeojson where name = '') to stdout DELIMITER ',' HEADER CSV;
+    COPY (select ST_AsText(wkb_geometry), name, way from ogrgeojson where name = '' order by random()) to stdout DELIMITER ',' HEADER CSV;
 " | psql -U postgres tigerdelta > tigerdelta-tasks/tigerdelta-noname.csv

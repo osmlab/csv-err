fc0d01a061bea622d6b489624a1ca289aa1caeb4
Tom Lee <tlee@mapbox.com>
2014-10-16 19:27:50 +0000

    moving install script to ksh style as well

--- src/install.sh
+++ src/install.sh
@@ -1,18 +1,18 @@
-set -e -u
+#set -e -u
 
 # detect platform
 unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
+if [ "$unamestr" = 'Darwin' ]; then
    platform='osx'
-elif [[ "$unamestr" == 'Linux' ]]; then
+elif [ "$unamestr" = 'Linux' ]; then
    platform='linux'
 fi
 
 # handle dependencies
-if [[ "$platform" == 'osx' ]]; then
+if [ "$platform" = 'osx' ]; then
    echo '- [OS X] Assuming you already have dependencies installed...'
    pg_user=`whoami`
-elif [[ "$platform" == 'linux' ]]; then
+elif [ "$platform" = 'linux' ]; then
    apt-get -y update
 
    echo "- installing postgres + postgis"
@@ -40,7 +40,7 @@ sudo -u $pg_user psql -U postgres -d template_postgis -c "GRANT ALL ON geography
 sudo -u $pg_user psql -U postgres -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
 
-if [[ "$platform" == 'linux' ]]; then
+if [ "$platform" = 'linux' ]; then
 
    # play nicely with other users
    sh -c 'echo "
